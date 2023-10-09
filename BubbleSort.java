47bc0444b1cec030687faa1c0da4ec14a45f1db4
Akash-Chaudhari-03 <130284760+Akash-Chaudhari-03@users.noreply.github.com>
2023-10-10 01:58:05 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,32 @@
+
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    static void sort(int [] arrA){
+        if(arrA==null || arrA.length==0)
+            return;
+
+        System.out.println("Original Array: " + Arrays.toString(arrA));
+        int size = arrA.length;
+        for (int i = 0; i <size-1 ; i++) {
+            for (int j = 0; j <size-i-1 ; j++) {
+                //check the adjacent elements
+                if(arrA[j]>arrA[j+1]){
+                    //swap the elements
+                    int temp = arrA[j];
+                    arrA[j] = arrA[j+1];
+                    arrA[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: " +  Arrays.toString(arrA));
+    }
+
+    public static void main(String[] args) {
+        int [] arrA = {22, 43, 2, 34, 99, 23, 11, 100};
+        sort(arrA);
+    }
+}
+ 
